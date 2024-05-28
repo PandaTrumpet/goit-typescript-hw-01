61e73ca8ff0a796ffee706d0d7bbd088e1f2e787
PandaTrumpet <dsavontrumpet@gmail.com>
2024-05-28 16:39:07 +0300

    update

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 async function fetchData<T>(url: string): Promise<T> {
   try {
     const response = await axios.get(url);
-    return response.data;
+    return <T>response.data;
   } catch (error) {
     throw new Error(`Error fetching from ${url}: ${error}`);
   }
--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,3 +1,3 @@
-function merge(objA: object, objB: object): object {
+function merge<T extends object, U extends object>(objA: T, objB: U) {
   return Object.assign(objA, objB);
 }
